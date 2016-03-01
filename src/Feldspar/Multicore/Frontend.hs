f042727282f3e7153b103282806831501c6e48cb
Karácsony Máté <k_mate@inf.elte.hu>
2016-03-01 19:25:44 +0100

    First version of multi-core frontend

--- src/Feldspar/Multicore/Frontend.hs
+++ src/Feldspar/Multicore/Frontend.hs
@@ -1,2 +1,23 @@
 module Feldspar.Multicore.Frontend where
 
+import Control.Monad.Operational.Higher
+
+import Feldspar
+import Feldspar.Multicore.Representation
+
+
+alloc :: CoreId -> Size -> AllocHost (LocalArr a)
+alloc coreId size = singleInj $ Alloc coreId size
+
+runHost :: Host a -> AllocHost a
+runHost = singleInj . RunHost
+
+
+fetch :: LocalArr a -> Range -> Arr a -> Host ()
+fetch localArr range = Host . singleInj . Fetch localArr range
+
+flush :: LocalArr a -> Range -> Arr a -> Host ()
+flush localArr range = Host . singleInj . Flush localArr range
+
+onCore :: CoreId -> Comp () -> Host ()
+onCore coreId = Host . singleInj . OnCore coreId
