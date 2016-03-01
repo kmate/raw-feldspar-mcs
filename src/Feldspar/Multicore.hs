f32dacd563aee98e6b279ad21a2ff0c4e786f85f
Karácsony Máté <k_mate@inf.elte.hu>
2016-03-01 19:25:59 +0100

    Exporting common modules

--- src/Feldspar/Multicore.hs
+++ src/Feldspar/Multicore.hs
@@ -1,2 +1,7 @@
-module Feldspar.Multicore where
+module Feldspar.Multicore
+  ( module Feldspar.Multicore.Frontend
+  , module Feldspar.Multicore.Representation
+  ) where
 
+import Feldspar.Multicore.Frontend
+import Feldspar.Multicore.Representation (AllocHost, Host, LocalArr)
