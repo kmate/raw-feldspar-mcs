71aefe8eab85372b14cbc11e80be9d0128a86b89
Karácsony Máté <k_mate@inf.elte.hu>
2016-03-18 21:03:16 +0100

    Exporting shared array type

--- src/Feldspar/Multicore.hs
+++ src/Feldspar/Multicore.hs
@@ -11,4 +11,4 @@ import Feldspar.Multicore.Compile.Parallella as F
 import Feldspar.Multicore.Frontend as F
 import Feldspar.Multicore.Pipe as F
 import Feldspar.Multicore.Reference as F
-import Feldspar.Multicore.Representation as F (CoreId, Size, IndexRange, LocalArr, CoreComp, Multicore, Host)
+import Feldspar.Multicore.Representation as F (CoreId, Size, IndexRange, LocalArr, SharedArr, CoreComp, Multicore, Host)
