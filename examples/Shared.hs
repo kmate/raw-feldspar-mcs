d13de4e51e3ee631d94d68c8287412df4dd744e3
Karácsony Máté <k_mate@inf.elte.hu>
2016-03-22 15:53:15 +0100

    Better example with shared memory channels between cores and host

--- examples/Shared.hs
+++ examples/Shared.hs
@@ -1,50 +1,69 @@
 module Shared where
 
+import qualified Prelude
+
 import Feldspar.Multicore
 
 
+n :: Word32
+n = 4
+
 shared :: Multicore ()
 shared = do
-    d0 :: SharedArr Int32 <- allocSArr 10
-    d1 :: SharedArr Int32 <- allocSArr 10
-    d2 :: SharedArr Int32 <- allocSArr 10
+    f0 <- allocRef 0
+    b0 <- allocSArr n
+    f1 <- allocRef 1
+    b1 <- allocLArr 1 n
+    f2 <- allocRef 2
+    b2 <- allocSArr n
     onHost $ do
-        input :: Arr Int32 <- newArr 10
-        for (0, 1, Incl 9) $ \i -> do
-            printf "Item %d> " i
-            item <- lift $ fget stdin
-            setArr i item input
+        writeRef f0 false
+        writeRef f1 false
+        writeRef f2 false
+        onCore 0 (f (f0, b0) (f1, b1))
+        onCore 1 (g (f1, b1) (f2, b2))
 
-        writeArr d0 (0,9) input
-        onCore 0 (f d0 d1)
-        onCore 1 (g d1 d2)
-        output <- newArr 10
-        readArr d2 (0,9) output
+        while (return $ true) $ do
+            input :: Arr Int32 <- newArr $ value n
+            for (0, 1, Excl $ value n) $ \i -> do
+                item <- lift $ fget stdin
+                setArr i item input
 
-        printf "Output:"
-        for (0, 1, Incl 9) $ \i -> do
-            item :: Data Int32 <- getArr i output
-            printf " %d" item
-        printf "\n"
+            writeArr b0 (0, value $ n - 1) input
+            writeRef f0 true
+            output <- newArr $ value n
+            while (not <$> readRef f2) $ return ()
+            writeRef f2 false
+            readArr b2 (0, value $ n - 1) output
+
+            for (0, 1, Excl $ value n) $ \i -> do
+                item :: Data Int32 <- getArr i output
+                printf "> %d\n" item
 
 
-f :: SharedArr Int32 -> SharedArr Int32 -> CoreComp ()
-f input output = do
+f :: (LocalRef Bool, SharedArr Int32) -> (LocalRef Bool, LocalArr Int32) -> CoreComp ()
+f (ri, input) (ro, output) = forever $ do
+    while (not <$> getLocalRef ri) $ return ()
+    setLocalRef ri false
+
     tmp <- newArr 10
     readArr input (0,9) tmp
-    for (0, 1, Incl 9) $ \i -> do
+    for (0, 1, Excl $ value n) $ \i -> do
         item :: Data Int32 <- getArr i tmp
-        setArr i (item + 1) tmp
-    writeArr output (0,9) tmp
+        setArr i (item + 1) -< output
+    setLocalRef ro true
 
-g :: SharedArr Int32 -> SharedArr Int32 -> CoreComp ()
-g input output = do
+g :: (LocalRef Bool, LocalArr Int32) -> (LocalRef Bool, SharedArr Int32) -> CoreComp ()
+g (ri, input) (ro, output) = forever $ do
+    while (not <$> getLocalRef ri) $ return ()
+    setLocalRef ri false
+
     tmp <- newArr 10
-    readArr input (0,9) tmp
     for (0, 1, Incl 9) $ \i -> do
-        item :: Data Int32 <- getArr i tmp
+        item :: Data Int32 <- getArr i -< input
         setArr i (item * 2) tmp
     writeArr output (0,9) tmp
+    setLocalRef ro true
 
 
 ------------------------------------------------------------
