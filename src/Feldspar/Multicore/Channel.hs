d7dd5342880aa406ae8cbb3df6691c6f4b431d8e
Karácsony Máté <k_mate@inf.elte.hu>
2016-05-06 17:19:38 +0200

    Refactoring + preparation for sending vectors over pipe-based channels

--- src/Feldspar/Multicore/Channel.hs
+++ src/Feldspar/Multicore/Channel.hs
@@ -1,62 +1,139 @@
-{-# LANGUAGE AllowAmbiguousTypes#-}
+{-# LANGUAGE UndecidableInstances #-}
 module Feldspar.Multicore.Channel where
 
 import Feldspar hiding ((==))
 import Feldspar.Multicore.Frontend
 import Feldspar.Multicore.Pipe
 import Feldspar.Multicore.Representation
+import Feldspar.Vector hiding (ofLength, VecChanSizeSpec)
 
+
+--------------------------------------------------------------------------------
+-- Channel interface
+--------------------------------------------------------------------------------
 
 host :: CoreId
 host = sharedId
 
-data Chan a where
-    HostToCoreChan :: ChanType a => HostToCorePipe (ChanElemType a) -> Chan a
-    CoreToHostChan :: ChanType a => CoreToHostPipe (ChanElemType a) -> Chan a
-    CoreToCoreChan :: ChanType a => CorePipe       (ChanElemType a) -> Chan a
+data Chan a
+  where
+    HostToCoreChan :: ChanType a
+                   => SizeSpec a
+                   -> HostToCorePipe (ChanElemType a)
+                   -> Chan a
+
+    CoreToHostChan :: ChanType a
+                   => SizeSpec a
+                   -> CoreToHostPipe (ChanElemType a)
+                   -> Chan a
+
+    CoreToCoreChan :: ChanType a
+                   => SizeSpec a
+                   -> CorePipe (ChanElemType a)
+                   -> Chan a
 
 class ChanType a
   where
     type ChanElemType a :: *
-    newChan :: CoreId -> CoreId -> Length -> Multicore (Chan a)
+    type SizeSpec a :: *
+    newChan :: CoreId -> CoreId -> SizeSpec a -> Multicore (Chan a)
 
-class ChanType a => Transferable m a
+class ChanType a => Transferable' m a
   where
     readChan :: Chan a -> m a
     writeChan :: Chan a -> a -> m ()
 
+class    (Transferable' Host a, Transferable' CoreComp a) => Transferable a
+instance (Transferable' Host a, Transferable' CoreComp a) => Transferable a
+
+
+data VecChanSizeSpec = VecChanSizeSpec Length Length
+
+ofLength :: Length -> Length -> VecChanSizeSpec
+ofLength = VecChanSizeSpec
+
+
+--------------------------------------------------------------------------------
+-- Instances
+--------------------------------------------------------------------------------
+
 instance PrimType a => ChanType (Data a)
   where
     type ChanElemType (Data a) = a
+    type SizeSpec (Data a)     = Length
     newChan from to l
         | host == from = do
             p :: HostToCorePipe a <- allocHostPipe to l
             onHost $ initPipe p
-            return $ HostToCoreChan p
+            return $ HostToCoreChan l p
         | host == to   = do
             p :: CoreToHostPipe a <- allocHostPipe from l
             onHost $ initPipe p
-            return $ CoreToHostChan p
+            return $ CoreToHostChan l p
         | otherwise = do
             p <- allocCorePipe from to l
             onHost $ initPipe p
-            return $ CoreToCoreChan p
+            return $ CoreToCoreChan l p
 
-instance PrimType a => Transferable Host (Data a)
+instance PrimType a => Transferable' Host (Data a)
   where
-    readChan (CoreToHostChan p) = do
+    readChan (CoreToHostChan _ p) = do
         arr <- newArr 1
         pullPipe p (0,0) arr
         elem <- getArr 0 arr
         return elem
-    writeChan (HostToCoreChan p) v = do
+    writeChan (HostToCoreChan _ p) v = do
         arr <- newArr 1
         setArr 0 v arr
         pushPipe p (0,0) arr
 
-instance PrimType a => Transferable CoreComp (Data a)
+instance PrimType a => Transferable' CoreComp (Data a)
   where
-    readChan (HostToCoreChan p) = readPipe p
-    readChan (CoreToCoreChan p) = readPipe p
-    writeChan (CoreToHostChan p) v = writePipe v p
-    writeChan (CoreToCoreChan p) v = writePipe v p
+    readChan (HostToCoreChan _ p) = readPipe p
+    readChan (CoreToCoreChan _ p) = readPipe p
+    writeChan (CoreToHostChan _ p) v = writePipe v p
+    writeChan (CoreToCoreChan _ p) v = writePipe v p
+
+
+instance PrimType a => ChanType (Vector (Data a))
+  where
+    type ChanElemType (Vector (Data a)) = a
+    type SizeSpec     (Vector (Data a)) = VecChanSizeSpec
+    newChan from to s@(VecChanSizeSpec n m)
+        | host == from = do
+            p :: HostToCorePipe a <- allocHostPipe to l
+            onHost $ initPipe p
+            return $ HostToCoreChan s p
+        | host == to   = do
+            p :: CoreToHostPipe a <- allocHostPipe from l
+            onHost $ initPipe p
+            return $ CoreToHostChan s p
+        | otherwise = do
+            p <- allocCorePipe from to l
+            onHost $ initPipe p
+            return $ CoreToCoreChan s p
+      where
+        l = n * m
+
+
+instance PrimType a => Transferable' Host (Vector (Data a))
+  where
+{-
+    readChan (CoreToHostChan _ p) = do
+        arr <- newArr 1
+        pullPipe p (0,0) arr
+        elem <- getArr 0 arr
+        return elem
+    writeChan (HostToCoreChan _ p) v = do
+        arr <- newArr 1
+        setArr 0 v arr
+        pushPipe p (0,0) arr
+-}
+instance PrimType a => Transferable' CoreComp (Vector (Data a))
+  where
+{-
+    readChan (HostToCoreChan _ p) = readPipe p
+    readChan (CoreToCoreChan _ p) = readPipe p
+    writeChan (CoreToHostChan _ p) v = writePipe v p
+    writeChan (CoreToCoreChan _ p) v = writePipe v p
+-}
